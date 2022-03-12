4e10f15420bda0399112bb07399e7e5a5d249dde
Hardiansa <hardiansya94@gmail.com>
2022-03-12 21:58:21 +0700

    feat: add animation to product list

--- src/features/ProductList/ProductListContainer.tsx
+++ src/features/ProductList/ProductListContainer.tsx
@@ -7,16 +7,53 @@ import {
   Grid,
   SimpleGrid,
   GridItem,
+  SimpleGridProps,
+  GridItemProps,
+  HeadingProps,
+  ImageProps
 } from "@chakra-ui/react";
 import { ProductData } from "@/utils/constant";
 import { formatPrice } from "@/utils/helper";
 import Link from "next/link";
+import { motion, Variants, HTMLMotionProps } from "framer-motion";
+
+type Merge<P, T> = Omit<P, keyof T> & T;
+type MotionHeadingProps = Merge<HeadingProps, HTMLMotionProps<"h2">>;
+type MotionGridItemProps = Merge<GridItemProps, HTMLMotionProps<"div">>;
+type MotionSimpleGridProps = Merge<
+  SimpleGridProps,
+  HTMLMotionProps<"div">
+>;
+type MotionImageProps = Merge<ImageProps, HTMLMotionProps<"img">>;
+
+const MotionHeading: React.FC<MotionHeadingProps> = motion(Heading);
+const MotionSimpleGrid: React.FC<MotionSimpleGridProps> = motion(SimpleGrid);
+const MotionGridItem: React.FC<MotionGridItemProps> = motion(GridItem);
+const MotionImage: React.FC<MotionImageProps> = motion(Image);
 
 interface ProductCardProps {
   thumbnail: string;
   title: string;
   price: string;
 }
+
+const easing = [0.16, 1, 0.3, 1];
+
+const SimpleGridVariants: Variants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2,
+      delayChildren: 0.2
+    },
+  },
+};
+
+const GridItemVariants: Variants = {
+  hidden: { opacity: 0, y: 50 },
+  show: { opacity: 1, y: 0, transition: { ease: easing } },
+};
 
 // eslint-disable-next-line react/display-name
 const ProductCard = forwardRef(
@@ -32,7 +69,7 @@ const ProductCard = forwardRef(
       as="a"
       {...rest}
     >
-      <Image src={thumbnail} alt={title} w={185} h={185}/>
+      <MotionImage src={thumbnail} alt={title} w={185} h={185} initial={{ x: 60, opacity: 0 }}  animate={{ x: 0, opacity: 1, transition: { delay: .6 } }} />
       <Grid gridTemplateColumns="4fr 1fr" mt="3">
         <Text fontWeight="semibold">{title}</Text>
         <Text>{price}</Text>
@@ -51,16 +88,30 @@ export const ProductListContainer = () => {
       justify="center"
       columnGap="84"
     >
-      <Heading fontSize="24">Select a Coffee :</Heading>
-      <SimpleGrid columns={3} width={762} columnGap="6">
+      <MotionHeading
+        fontSize="24"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, ease: easing }}
+      >
+        Select a Coffee
+      </MotionHeading>
+      <MotionSimpleGrid
+        columns={3}
+        width={762}
+        columnGap="6"
+        variants={SimpleGridVariants}
+        initial="hidden"
+        animate="show"
+      >
         {ProductData.map(({ slug, sizes, ...rest }) => (
-          <GridItem key={slug}>
+          <MotionGridItem variants={GridItemVariants} whileHover={{ scale: 1.05 }} whileTap={{ scale: .95 }} key={slug}>
             <Link href={`/product/${slug}`} passHref>
-              <ProductCard {...rest} price={ formatPrice(sizes[1].price)} />
+              <ProductCard price={formatPrice(sizes[1].price)} {...rest} />
             </Link>
-          </GridItem>
+          </MotionGridItem>
         ))}
-      </SimpleGrid>
+      </MotionSimpleGrid>
     </Flex>
   );
 };
