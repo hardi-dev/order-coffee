86defb1e3eabc05e22296f7479db8be1067c716d
Hardiansa <hardiansya94@gmail.com>
2022-03-12 23:02:51 +0700

    feat: add animation to product details

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@/theme/theme";
+import { AnimatePresence } from "framer-motion";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
   return <ChakraProvider theme={theme} resetCSS>
-     <Component {...pageProps} />
+    <AnimatePresence exitBeforeEnter>
+      <Component {...pageProps} key={router.route}/>
+    </AnimatePresence>
   </ChakraProvider>
 }
 
